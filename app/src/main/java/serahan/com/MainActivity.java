c7865cca1d249f0536046ce9a7e827fa613129a5
은지 정 <seyrahan22@gmail.com>
2019-07-01 17:48:22 +0900

    네이버 지도 연결 - 마커캡션 5-2. 마커 캡션 연습 마침.

--- app/src/main/java/serahan/com/MainActivity.java
+++ app/src/main/java/serahan/com/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PointF;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -19,7 +21,9 @@ import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Align;
+import com.naver.maps.map.overlay.InfoWindow;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
 import com.naver.maps.map.util.MarkerIcons;
@@ -114,10 +118,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // uiSettings.setTiltGesturesEnabled(false); // 틸트 제스처 비활성화 ( 틸트 : 두개의 손가락으로 지도 위아래 드래그 -> 기울임 각도 변경)
         // uiSettings.setRotateGesturesEnabled(false); // 회전 제스처 비활성화
 
-        naverMap.setOnMapClickListener((pointF, coord) -> Toast.makeText(this, "클릭 : " + coord.latitude + ", " + coord.longitude,  Toast.LENGTH_SHORT).show());
-        naverMap.setOnMapLongClickListener(((pointF, coord) -> Toast.makeText(this, "롱 클릭 : " + coord.latitude + ", " + coord.longitude, Toast.LENGTH_SHORT).show()));
+        // naverMap.setOnMapClickListener((pointF, coord) -> Toast.makeText(this, "클릭 : " + coord.latitude + ", " + coord.longitude,  Toast.LENGTH_SHORT).show());
+        // naverMap.setOnMapLongClickListener(((pointF, coord) -> Toast.makeText(this, "롱 클릭 : " + coord.latitude + ", " + coord.longitude, Toast.LENGTH_SHORT).show()));
 
-        naverMap.setOnMapClickListener(((pointF, coord) -> Toast.makeText(this,"지도 클릭", Toast.LENGTH_SHORT).show()));
+
 
         // ############ 심볼 클릭 ################
         naverMap.setOnSymbolClickListener(symbol -> {
@@ -196,5 +200,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Follow : 위치 추적 활성화 O, 현위치 오버레이와 카메라의 좌표가 사용자 위치 따라 이동. 카메라 건드리면 NoFollow로 전환
         // Face : 위치 추적 활성화 O, 현위치 오버레이, 카메라의 좌표, 베어링이 사용자의 위치 및 방향 따라 이동. 카메라 건드리면 NoFollow로 전환
         naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+
+        // #########################################################
+        // 정보 창
+        InfoWindow infoWindow = new InfoWindow();
+        infoWindow.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "Here!";
+            }
+        });
+        infoWindow.open(marker2);
+
+
+
+        // ############################################################
+        // 지도를 클릭하면 정보 창을 닫음
+        naverMap.setOnMapClickListener(((pointF, latLng) -> {
+            infoWindow.close();
+        }));
+
+        // 마커를 클릭하면 :
+        Overlay.OnClickListener listener = overlay -> {
+            Marker marker4 = (Marker)overlay;
+
+            if(marker4.getInfoWindow() == null) {
+                infoWindow.open(marker4);
+            } else {
+                infoWindow.close();
+            }
+            return true;
+        };
+
+        marker.setOnClickListener(listener);
+        marker2.setOnClickListener(listener);
+        marker3.setOnClickListener(listener);
+
     }
 }
