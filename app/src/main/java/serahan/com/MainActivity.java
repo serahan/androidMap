25169a12701d6abc47ab3f84002fc798cf006733
은지 정 <seyrahan22@gmail.com>
2019-07-01 11:05:55 +0900

    네이버 지도 연결 3. 줌/카메라 뷰 연습 완료

--- app/src/main/java/serahan/com/MainActivity.java
+++ app/src/main/java/serahan/com/MainActivity.java
@@ -39,22 +39,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng location = new LatLng(35.9424938, 126.683274); // 군산대학교 아카데미홀 위치
 
         // 카메라 위치와 줌 조절 (숫자가 클수록 확대)
-        CameraPosition cameraPosition = new CameraPosition(location,17);
-        naverMap.setCameraPosition(cameraPosition);
+        //CameraPosition cameraPosition = new CameraPosition(location,17);
+        //naverMap.setCameraPosition(cameraPosition);
 
-        /* CameraPosition cameraPosition = new CameraPosition(
+        CameraPosition cameraPosition = new CameraPosition(
                 new LatLng(37.5666102, 126.9783881),
                 16,     // 줌 레벨
-                20,     // 기울임 각도
-                180     // 베어링 각도
+                45,     // 기울임 각도
+                0     // 베어링 각도
         );
-
-        Toast.makeText(context, " 대상 지정 위도 : " + cameraPosition.target.latitude + ", " +
-                                "대상 지점 경도 : " +cameraPosition.target.longitude + ", " +
-                                "줌 레벨 : " + cameraPosition.zoom + ", " +
-                                "기울임 각도 : " + cameraPosition.tilt + ", " +
-                                "베어링 각도 : " + cameraPosition.bearing,
-                                Toast.LENGTH_SHORT).show(); */
+        naverMap.setCameraPosition(cameraPosition);
 
         // 줌 범위 제한
         naverMap.setMinZoom(5.0);
